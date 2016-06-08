3d4bfebfd5750953d34dcbf9c0601dfbf52cdee2
VolodymyrSydorov <wladimir.sidorow@gmail.com>
2016-06-08 23:52:15 +0300

    url changed

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,6 +1,6 @@
 angular.module("testTaskApp", ["ngRoute", 'ngMaterial'])
-    /*.constant("baseUrl", "https://infinite-depths-56882.herokuapp.com/")*/
-    .constant("baseUrl", "http://localhost:3000")
+    .constant("baseUrl", "http://aqueous-plains-11730.herokuapp.com/")
+    /*.constant("baseUrl", "http://localhost:3000")*/
     .controller("ipCtrl", ['$scope', '$location', '$timeout', '$mdToast', function ($scope, $location, $timeout, $mdToast) {
 
 
