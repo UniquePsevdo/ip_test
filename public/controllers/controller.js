2b43b4d1ce3508b7b08e094a0b02c5527ed0b142
VolodymyrSydorov <wladimir.sidorow@gmail.com>
2016-06-12 10:42:03 +0300

    pages

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,7 +1,26 @@
 angular.module("testTaskApp", ["ngRoute", 'ngMaterial'])
-    .constant("baseUrl", "http://aqueous-plains-11730.herokuapp.com/")
-    /*.constant("baseUrl", "http://localhost:3000")*/
-    .controller("ipCtrl", ['$scope', '$location', '$timeout', '$mdToast', function ($scope, $location, $timeout, $mdToast) {
+    /*.constant("baseUrl", "http://aqueous-plains-11730.herokuapp.com/")*/
+    .constant("baseUrl", "http://localhost:3000")
+    .controller("ipCtrl", ['$scope', '$location', '$timeout', '$mdToast', '$window', function ($scope, $location, $timeout, $mdToast, $window) {
 
+        $scope.user = {
+            name:       "",
+            email:      "",
+            city:       "",
+            country:    "",
+            gender:     ""
+        };
+
+        $scope.redirectVotingPage = function(){
+            $window.location.href = '/voting';
+        }
+
+        $scope.redirectApprovePage = function(){
+            $window.location.href = '/approve';
+        }
+
+        $scope.redirectLastPage = function(){
+            $window.location.href = '/last';
+        }
 
     }]);
