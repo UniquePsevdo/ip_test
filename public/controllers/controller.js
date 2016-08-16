4c8b0d3b18ea2012b7a2ea20250a3cfcc24f154e
VolodymyrSydorov <wladimir.sidorow@gmail.com>
2016-08-16 23:41:07 +0300

    change

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -173,10 +173,10 @@ angular.module("testTaskApp", ["ngRoute", 'ngMaterial', 'appServices'])
             for(var i = 0; i < $scope.us_voted.length; i++){
                 for(var j=0; j < $scope.us_providers_data.length; j++){
                     if($scope.us_voted[i].cityId.trim()===$scope.us_providers_data[j].name.trim()){
-                        console.log('1');
+                        console.log(i,'-1');
                         for(var z=0; z < $scope.us_providers_result.length; z++){
                             if($scope.us_providers_result[z].name.trim()===$scope.us_providers_data[j].provider.trim()){
-                                console.log('2');
+                                console.log(i,'-2');
                                 $scope.us_providers_result[z].result.push($scope.us_voted[i].email);
                                 $scope.us_providers_result[z].currentNumber = $scope.us_providers_result[z].result.length;
                             }
@@ -184,7 +184,7 @@ angular.module("testTaskApp", ["ngRoute", 'ngMaterial', 'appServices'])
                         }
                         for(var z=0; z < $scope.us_cities_result.length; z++){
                             if($scope.us_cities_result[z].name.trim()===$scope.us_providers_data[j].city.trim()){
-                                console.log('3');
+                                console.log(i,'-3');
                                 $scope.us_cities_result[z].result.push($scope.us_voted[i].email);
                                 $scope.us_cities_result[z].currentNumber = $scope.us_cities_result[z].result.length;
                             }
@@ -193,7 +193,6 @@ angular.module("testTaskApp", ["ngRoute", 'ngMaterial', 'appServices'])
                     }
                     break;
                 }
-                console.log(i);
             }
 
             console.log($scope.us_providers_result);
